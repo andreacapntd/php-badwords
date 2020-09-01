9772c61885ff2f3ee9da00af852fdf063847c73a
andreacapntd <andreacapntd@gmail.com>
2020-09-01 17:02:32 +0200

    ex php badword finito

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
  <?php
 
+  $word =$_GET['word'];
+  $new_word = '***';
   $paragraph = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
   ?>
@@ -10,6 +12,11 @@
     <?php
 
      var_dump($paragraph);
+     $new_paragraph = str_replace($word, $new_word, $paragraph );
+     echo '<br />';
+     echo '<br />';
+     echo $new_paragraph;
+
 
     ?>
   </p>
